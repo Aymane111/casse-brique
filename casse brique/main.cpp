ce1321698b7b142c48127836e32d7d873df50987
Aymane111 <60976408+Aymane111@users.noreply.github.com>
2021-01-02 21:23:35 +0100

    Update main.cpp

--- casse brique/main.cpp
+++ casse brique/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#include "graphics"
+#include "constVar.h"
 
 using namespace std;
 
 int main()
-{
-    cout << "Hello world!" << endl;
-    return 0;
+{   
+    setcolor( int 16 );
+    void bar(int 0, int 0, int RESOLUTION_X, int 40 ); //mur haut
+    void bar(int 0, int RESOLUTION_Y-40, int 40, int RESOLUTION_Y ); // mur gauche
+    void bar(int RESOLUTION_X-40, int 40, int RESOLUTION_X, int RESOLUTION_Y ); //mur a droite
+    
 }
