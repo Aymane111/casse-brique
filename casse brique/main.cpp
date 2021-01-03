4dddbfee1e870566a2d9f4c07527654ee8a79bab
Aymane111 <60976408+Aymane111@users.noreply.github.com>
2021-01-03 16:51:05 +0100

    Update main.cpp

--- casse brique/main.cpp
+++ casse brique/main.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
 #include "graphics.h"
-#include "constvar.h"
-
+#include "mur.h"
+#include "constVariables.h"
 using namespace std;
 
 int main()
 {
-     opengraphsize(RESOLUTION_X,RESOLUTION_Y);
-     setcolor (10);
-     bar (0,0,RESOLUTION_X,40); //mur haut
-     bar (0,40,40,RESOLUTION_Y ); // mur gauche
-     bar (RESOLUTION_X-40,40,RESOLUTION_X,RESOLUTION_Y ); //mur a droite
-     getch();
-     closegraph();
-
+    opengraphsize(800,600);
+    mur (0,0,RESOLUTION_X,40,10); //mur haut
+    mur (0,40,40,RESOLUTION_Y,10); //mur gauche
+    mur (RESOLUTION_X-40,40,RESOLUTION_X,RESOLUTION_Y,10); // mur à droite
+    getch();
+    closegraph();
 }
+
