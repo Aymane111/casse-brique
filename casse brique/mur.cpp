d3318ea51847e3ae595ab06009e785ebcc20e3e7
Aymane111 <60976408+Aymane111@users.noreply.github.com>
2021-01-03 16:52:31 +0100

    Update mur.cpp

--- casse brique/mur.cpp
+++ casse brique/mur.cpp
@@ -1,21 +1,23 @@
 #include "mur.h"
 #include "graphics.h"
-#include"constVariables.h"
-mur::mur(int largeur,int hauteur,int couleur): d_largeur{largeur},
+#include "constVariables.h"
+mur::mur(int xh,int yh,int largeur,int hauteur,int couleur):
+                                                d_xh{xh},
+                                                d_yh{yh},
+                                                d_largeur{largeur},
                                                 d_hauteur{hauteur},
                                                 d_couleur{couleur}
 {
-    void bar(int 0, int 0, int largeur, int hauteur );
+     setcolor(couleur);
+     bar(xh,yh,largeur,hauteur );
 }
-/* les trois murs à créer :
-Haut : mur (RESOLUTION_X,40,16)
-Gauche: mur (40,RESOLUTION_Y,16)
-Droit: mur (40,RESOLUTION_Y,16)
-*/
+
 mur::~mur()
 {
 
 }
+int xh()const {return d_xh;}
+int yh()const {return d_yh;}
 int largeur()const {return d_largeur;}
 int hauteur()const {return d_hauteur};
 int couleur()const {return d_couleur};
