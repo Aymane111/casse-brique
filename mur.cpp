96edba80fb6bdc3430593878bbe096891ca9a828
Aymane111 <60976408+Aymane111@users.noreply.github.com>
2021-01-04 23:07:45 +0100

    Add files via upload

---
+++ mur.cpp
@@ -0,0 +1,23 @@
+#include "mur.h"
+#include "../graphics.h"
+#include"constVariables.h"
+mur::mur(int xh,int yh,int largeur,int hauteur,int couleur):
+                                                d_xh{xh},
+                                                d_yh{yh},
+                                                d_largeur{largeur},
+                                                d_hauteur{hauteur},
+                                                d_couleur{couleur}
+{
+     setcolor(couleur);
+     bar(xh,yh,largeur,hauteur );
+}
+
+mur::~mur()
+{
+
+}
+int xh()const {return d_xh;}
+int yh()const {return d_yh;}
+int largeur()const {return d_largeur;}
+int hauteur()const {return d_hauteur};
+int couleur()const {return d_couleur};
---
+++ mur.h
@@ -0,0 +1,25 @@
+#ifndef MUR_H
+#define MUR_H
+
+
+class mur
+{
+    public:
+        mur(int xh,int yh,int largeur,int hauteur,int couleur);
+        virtual ~mur();
+        int xh()const;
+        int yh()const;
+        int largeur()const;
+        int hauteur()const;
+        int couleur()const;
+    protected:
+
+    private:
+        int d_xh;
+        int d_yh;
+        int d_largeur;
+        int d_hauteur;
+        int d_couleur;
+};
+
+#endif // MUR_H
